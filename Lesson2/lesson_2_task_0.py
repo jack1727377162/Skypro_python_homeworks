8d02cbc581e9fd4964d691ea9ed2ba9805ce5732
jack1727377162 <eger35881@gmail.com>
2024-07-22 09:49:27 +0300

    Корректировка задания 1

--- Lesson2/lesson_2_task_0.py
+++
@@ -1,5 +0,0 @@
-my_age= input("Сколько вам лет? ")
-print("Your age: " + my_age)
-my_age = int(my_age)
-my_age = my_age + 1
-print("В следующем году вам будет:", my_age)
--- Lesson2/lesson_2_task_8.py
+++
@@ -1,2 +0,0 @@
-number = list(range(18, 1, -4))
-print(number)
